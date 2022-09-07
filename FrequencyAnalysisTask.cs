2bbaee8cee1e5f85760e342204c42a6f9f10cb7c
Otabek Sh <otabek.pro@hotmail.com>
2022-09-07 14:34:37 +0300

    added a new TryAdd method

--- FrequencyAnalysisTask.cs
+++ FrequencyAnalysisTask.cs
@@ -1,25 +1,84 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Net.Http.Headers;
 
 namespace TextAnalysis
 {
     static class FrequencyAnalysisTask
     {
+        
+        public static bool TryAdd<TKey, TValue>(this IDictionary<TKey, TValue> dictionary, TKey key, TValue value)
+        {
+            if (dictionary == null)
+            {
+                throw new ArgumentNullException(nameof(dictionary));
+            }
+
+            if (!dictionary.ContainsKey(key))
+            {
+                dictionary.Add(key, value);
+                return true;
+            }
+
+            return false;
+        }
+
+        public static void TryAdd(this Dictionary<(string, string), int> dictionary, (string, string) key)
+        {
+            if (dictionary == null)
+            {
+                throw new ArgumentNullException(nameof(dictionary));
+            }
+
+            if (dictionary.ContainsKey(key))
+            {
+                int n = 0;
+                dictionary.TryGetValue(key, out n);
+                dictionary.Remove(key);
+                dictionary.Add(key,++n);
+            }
+            else
+            {
+                dictionary.Add(key, 1);
+            }
+          
+        }
+
+        public static void CreateResultMap(Dictionary<string, string> result, Dictionary<(string, string), int> frequencyMap)
+        {
+            foreach(var e in frequencyMap.Keys)
+            {
+
+            }
+        }
+
         public static Dictionary<string, string> GetMostFrequentNextWords(List<List<string>> text)
         {
             var result = new Dictionary<string, string>();
+            var frequencyMap = new Dictionary<(string, string), int>();
 
-            int i = 1;
+
+
             foreach (var word in text)
             {
-                Console.WriteLine("String " + i);
-                foreach (var word2 in word)
+                for (int i = 0; i < word.Count - 1; i++)
                 {
-                    Console.Write(word2 + " ");
+                    if (word.Count >= 2)
+                        TryAdd(frequencyMap, (word[i], word[i + 1]));
+                    else
+                        break;
                 }
-                Console.WriteLine("\n" + new String('-', 20));
-                i++;
+
+                for (int i = 0; i < word.Count - 2; i++)
+                {
+                    if (word.Count >= 3)
+                        TryAdd(frequencyMap, ($"{word[i]} {word[i + 1]}", word[i+2]) );
+                    else
+                        break;
+                }
             }
+            
+            
             return result;
         }
     }
