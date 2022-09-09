45d28f130de7d7bebf96db3913f82f1cc0d06569
Otabek Sh <otabek.pro@hotmail.com>
2022-09-09 22:06:25 +0300

    Алгоритм работает правильно и эффективно! Измененил string.Compare на string.CompareOrdinal

--- FrequencyAnalysisTask.cs
+++ FrequencyAnalysisTask.cs
@@ -1,15 +1,15 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Net.Http.Headers;
-using System.Text;
 
 namespace TextAnalysis
 {
     static class FrequencyAnalysisTask
     {
-        
-        public static bool TryAdd<TKey, TValue>(this IDictionary<TKey, TValue> dictionary, TKey key, TValue value)
+
+        public static bool TryAdd<TKey, TValue>(this IDictionary<TKey, TValue> dictionary,
+            TKey key,
+            TValue value)
         {
             if (dictionary == null)
             {
@@ -25,7 +25,9 @@ namespace TextAnalysis
             return false;
         }
 
-        public static void TryAdd(this Dictionary<string, Dictionary<string, int>> dictionary, string key, string value)
+        public static void TryAdd(this Dictionary<string, Dictionary<string, int>> dictionary,
+            string key,
+            string value)
         {
             if (dictionary == null)
             {
@@ -35,7 +37,7 @@ namespace TextAnalysis
             if (dictionary.ContainsKey(key))
             {
 
-                if(dictionary[key].ContainsKey(value))
+                if (dictionary[key].ContainsKey(value))
                 {
                     dictionary[key][value]++;
                 }
@@ -46,15 +48,19 @@ namespace TextAnalysis
             }
             else
             {
-                dictionary.Add(key, new Dictionary<string, int>() { {value, 1 } });
+                dictionary.Add(key, new Dictionary<string, int>() { { value, 1 } });
             }
         }
 
-        public static void CreateResultMap(Dictionary<string, string> result, Dictionary<string, Dictionary<string, int>> frequencyMap)
+        public static void CreateResultMap(Dictionary<string, string> result,
+            Dictionary<string, Dictionary<string, int>> frequencyMap)
         {
-            foreach(var e in frequencyMap.Keys)
+            foreach (var e in frequencyMap.Keys)
             {
-                var tempResultFreqMap = new Dictionary<string, int>() { { frequencyMap[e].Keys.Min(), frequencyMap[e][frequencyMap[e].Keys.Min()] } };
+                var tempResultFreqMap = new Dictionary<string, int>() 
+                { 
+                    { frequencyMap[e].Keys.Min(), frequencyMap[e][frequencyMap[e].Keys.Min()] } 
+                };
 
                 foreach (var ee in frequencyMap[e])
                 {
@@ -65,18 +71,17 @@ namespace TextAnalysis
                     }
                     else if (ee.Value == tempResultFreqMap.Values.First())
                     {
-                        if (string.Compare(ee.Key, tempResultFreqMap.Keys.First()) < 0)
+                        if (string.CompareOrdinal(ee.Key, tempResultFreqMap.Keys.First()) < 0)
                         {
                             tempResultFreqMap.Clear();
                             tempResultFreqMap.Add(ee.Key, ee.Value);
                         }
                     }
-                } 
-                result.TryAdd(e,tempResultFreqMap.Keys.First());
+                }
+                result.TryAdd(e, tempResultFreqMap.Keys.First());
             }
         }
 
-        //----------------------------------------------------------------------------------------
         public static Dictionary<string, string> GetMostFrequentNextWords(List<List<string>> text)
         {
             var result = new Dictionary<string, string>();
@@ -87,7 +92,7 @@ namespace TextAnalysis
                 for (int i = 0; i < word.Count - 1; i++)
                 {
                     if (word.Count >= 2)
-                        TryAdd(frequencyMap, word[i], word[i+1]);
+                        TryAdd(frequencyMap, word[i], word[i + 1]);
                     else
                         break;
                 }
@@ -95,22 +100,37 @@ namespace TextAnalysis
                 for (int i = 0; i < word.Count - 2; i++)
                 {
                     if (word.Count >= 3)
-                        TryAdd(frequencyMap, $"{word[i]} {word[i + 1]}", word[i+2] );
+                        TryAdd(frequencyMap, $"{word[i]} {word[i + 1]}", word[i + 2]);
                     else
                         break;
                 }
             }
-
             CreateResultMap(result, frequencyMap);
-
-            foreach(var e in frequencyMap)
-            {
-                var lineBuil = new StringBuilder();
-
-                lineBuil.AppendLine($"{e} {{  }}");
-            }
-
             return result;
         }
+        /* //////// Визуализация и быстрый поиск по частотному словарю
+        private static async void WriteInTXT(Dictionary<string, Dictionary<string, int>> frequencyMap)
+        {
+            var lineBuil = new StringBuilder();
+            foreach (var e in frequencyMap.Keys)
+            {
+
+                lineBuil.AppendLine($"{e}");
+
+                foreach (var ee in frequencyMap[e])
+                {
+                    lineBuil.AppendLine($"\t{ee.Key}, {ee.Value}");
+                }
+                lineBuil.AppendLine(new string('_', 30));
+            }
+            string path = "note1.txt";
+
+            // полная перезапись файла 
+            using (StreamWriter writer = new StreamWriter(path, false))
+            {
+                await writer.WriteLineAsync(lineBuil.ToString());
+            }
+        }
+        */
     }
 }
